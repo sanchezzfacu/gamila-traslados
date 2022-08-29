db5fadd85237f36a660e7d5cb2d95bea7adbb298
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-08-29 18:10:12 -0300

    .

--- tiny-slider.js
+++ tiny-slider.js
@@ -2,17 +2,17 @@ var slider = tns({
     container: '.services-slider',
     'slideBy': 1,
     'speed' : 400,
-    'nav': false,
+    'nav': true,
     'items': 1.20,
     'autoplay': true,
-    'controlsPosition': 'bottom',
-    'controlsText': ['<', '>'],
-    'autoplayTimeout' : 3000,
-    'autoplayHoverPause': true,
+    'navPosition': 'bottom',
+    'autoplayTimeout' : 5000,
+    'autoplayHoverPause': false,
     autoplayButtonOutput: false,
+    arrowKeys: false,
     responsive: {
         660: {
-            'items': 1.5
+            'items': 1.20
         },
         900: {
             'items': 2.25
