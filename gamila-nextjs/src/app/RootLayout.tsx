3d40694b28d6c70004092b957180060112ead80d
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2023-06-13 01:30:58 -0300

    config nextjs proyect and add navbar

---
+++ gamila-nextjs/src/app/RootLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+
+function RootLayout({ children }: {children: ReactNode}) {
+
+  return (
+    <div className="">
+      
+    </div>
+  )
+}
+
+export default RootLayout
